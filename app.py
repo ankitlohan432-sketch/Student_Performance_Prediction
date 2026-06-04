8e3884998a11870d2c2a4ca682711681cdb0c34b
Ankit <ankitlohan432@gmail.com>
2026-06-04 15:39:05 +0530

    Strip whitespace from DataFrame columns and result

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('student_data.csv')
+
 df.columns = df.columns.str.strip()
 
 df['result'] = df['result'].str.lower().str.strip()
